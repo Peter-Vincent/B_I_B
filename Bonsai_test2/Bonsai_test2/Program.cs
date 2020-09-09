dbe605ec707d906726b50552b0e6ad36a2bd24f5
PeterVincent96 <45718646+PeterVincent96@users.noreply.github.com>
2020-09-09 18:14:21 +0100

    Cleaning code

--- Bonsai_test2/Bonsai_test2/Program.cs
+++ Bonsai_test2/Bonsai_test2/Program.cs
@@ -16,8 +16,10 @@ namespace Bonsai_test2
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => 
+            new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+            });
 
             await builder.Build().RunAsync();
         }
--- Bonsai_test2/Bonsai_test2/bonsai_func.cs
+++ Bonsai_test2/Bonsai_test2/bonsai_func.cs
@@ -32,22 +32,6 @@ namespace Bonsai_test2
         public Bonsai_code[] bonsai_Codes;
         public void Launch(string data)
         {
-
-
-            /*
-            var fileName = "Bonsai_files/trial.bonsai"; 
-            if (!File.Exists(fileName))
-            {
-                throw new ArgumentException("Specified workflow file does not exist.");
-            }
-            
-            
-            //var audio = workflowBuilder.Workflow.Add(new CombinatorBuilder { Combinator = new ScalarBuffer { Size = new Size(10,10) } });
-            var timer = workflowBuilder.Workflow.Add(new CombinatorBuilder { Combinator = new Timer { Period = TimeSpan.FromSeconds(1) } });
-            var output = workflowBuilder.Workflow.Add(new WorkflowOutputBuilder());
-
-            workflowBuilder.Workflow.AddEdge(timer, output, new ExpressionBuilderArgument());
-            */
             WorkflowBuilder workflowBuilder = new WorkflowBuilder();
             using (var reader = XmlReader.Create(new StringReader(data)))
 
@@ -58,16 +42,6 @@ namespace Bonsai_test2
             var observable = Expression.Lambda<Func<IObservable<Int64>>>(workflow).Compile();
             observable().Subscribe(x => value = (int)x);
             value += 42;
-            /*
-            
-
-            var workflowCompleted = new ManualResetEvent(false);
-            workflowBuilder.Workflow.BuildObservable().Subscribe(
-                unit => { },
-                ex => { workflowCompleted.Set(); },
-                () => workflowCompleted.Set());
-            workflowCompleted.WaitOne();
-            */
         }
     }
 }
