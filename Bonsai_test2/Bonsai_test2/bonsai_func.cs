f467b21f705e304a70d15d037540787003bcf36f
PeterVincent96 <45718646+PeterVincent96@users.noreply.github.com>
2020-09-10 18:23:46 +0100

    Adding Keybaord

--- Bonsai_test2/Bonsai_test2/bonsai_func.cs
+++ Bonsai_test2/Bonsai_test2/bonsai_func.cs
@@ -3,6 +3,8 @@ using Bonsai.Audio;
 using Bonsai.Dsp;
 using Bonsai.Expressions;
 using Bonsai.Reactive;
+using Bonsai.Windows.Input;
+using Bonsai.IO;
 using OpenCV.Net;
 using System;
 using System.Net.Http;
@@ -24,14 +26,33 @@ namespace Bonsai_test2
     public class Bonsai_func
     {
         public int value = 0;
+        public int value2 = 0;
 
         public void Change_val()
         {
             value++;
         }
         public Bonsai_code[] bonsai_Codes;
+        
+        public void Launch_key()
+        {
+            WorkflowBuilder workflowBuilder2 = new WorkflowBuilder();
+            var keypress = workflowBuilder2.Workflow.Add(new CombinatorBuilder { Combinator = new KeyDown { } });
+            var int_node = workflowBuilder2.Workflow.Add(new CombinatorBuilder { Combinator = new Bonsai.Expressions.Int64Property { Value = 1} });
+            var accumulate = workflowBuilder2.Workflow.Add(new CombinatorBuilder { Combinator = new Bonsai.Reactive.Accumulate { } });
+            var output   = workflowBuilder2.Workflow.Add(new WorkflowOutputBuilder());
+
+            workflowBuilder2.Workflow.AddEdge(keypress, int_node, new ExpressionBuilderArgument());
+            workflowBuilder2.Workflow.AddEdge(int_node, accumulate, new ExpressionBuilderArgument());
+            workflowBuilder2.Workflow.AddEdge(accumulate, output, new ExpressionBuilderArgument());
+            var workflow2 = workflowBuilder2.Workflow.Build();
+            var observable2 = Expression.Lambda<Func<IObservable<Int64>>>(workflow2).Compile();
+            observable2().Subscribe(x => value2 = (int)x);
+            value2 += 24;
+        }
         public void Launch(string data)
         {
+
             WorkflowBuilder workflowBuilder = new WorkflowBuilder();
             using (var reader = XmlReader.Create(new StringReader(data)))
 
