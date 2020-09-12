9d23243a060eccc15401dd780def085190186b6f
PeterVincent96 <45718646+PeterVincent96@users.noreply.github.com>
2020-09-12 19:55:13 +0100

    Add mouse stuff

--- Bonsai_test2/Bonsai_test2/bonsai_func.cs
+++ Bonsai_test2/Bonsai_test2/bonsai_func.cs
@@ -47,22 +47,42 @@ namespace Bonsai_test2
         }
         public Bonsai_code[] bonsai_Codes;
 
-        
+
+        /// <summary>
+        /// Code to get dynamic updating of the mous location and clicks
+        /// </summary>
+        public ExternalEventSource<MouseEventArgs> mouse_move = new ExternalEventSource<MouseEventArgs>();
+
+        public IObservable<MouseEventArgs> Launch_mouse()
+        {
+            WorkflowBuilder workflowBuilderMouse = new WorkflowBuilder();
+            var mouse = workflowBuilderMouse.Workflow.Add(new CombinatorBuilder { Combinator = mouse_move });
+            var output = workflowBuilderMouse.Workflow.Add(new WorkflowOutputBuilder());
+
+            workflowBuilderMouse.Workflow.AddEdge(mouse, output, new ExpressionBuilderArgument());
+            var workflow = workflowBuilderMouse.Workflow.Build();
+            var observable = Expression.Lambda<Func<IObservable<MouseEventArgs>>>(workflow).Compile();
+            return observable();
+        }
+
+
+        /// <summary>
+        /// Manual bonsai code to run the keyboard
+        /// </summary>
         public ExternalEventSource<KeyboardEventArgs> event_source = new ExternalEventSource<KeyboardEventArgs>();
-        
         public void Launch_key()
         {
              
-            WorkflowBuilder workflowBuilder2 = new WorkflowBuilder();
-            var keypress = workflowBuilder2.Workflow.Add(new CombinatorBuilder { Combinator = event_source });
-            var int_node = workflowBuilder2.Workflow.Add(new CombinatorBuilder { Combinator = new Bonsai.Expressions.Int64Property { Value = 1} });
-            var accumulate = workflowBuilder2.Workflow.Add(new CombinatorBuilder { Combinator = new Bonsai.Reactive.Accumulate { } });
-            var output   = workflowBuilder2.Workflow.Add(new WorkflowOutputBuilder());
+            WorkflowBuilder workflowBuilderKey = new WorkflowBuilder();
+            var keypress = workflowBuilderKey.Workflow.Add(new CombinatorBuilder { Combinator = event_source });
+            var int_node = workflowBuilderKey.Workflow.Add(new CombinatorBuilder { Combinator = new Bonsai.Expressions.Int64Property { Value = 1} });
+            var accumulate = workflowBuilderKey.Workflow.Add(new CombinatorBuilder { Combinator = new Bonsai.Reactive.Accumulate { } });
+            var output   = workflowBuilderKey.Workflow.Add(new WorkflowOutputBuilder());
 
-            workflowBuilder2.Workflow.AddEdge(keypress, int_node, new ExpressionBuilderArgument());
-            workflowBuilder2.Workflow.AddEdge(int_node, accumulate, new ExpressionBuilderArgument());
-            workflowBuilder2.Workflow.AddEdge(accumulate, output, new ExpressionBuilderArgument());
-            var workflow2 = workflowBuilder2.Workflow.Build();
+            workflowBuilderKey.Workflow.AddEdge(keypress, int_node, new ExpressionBuilderArgument());
+            workflowBuilderKey.Workflow.AddEdge(int_node, accumulate, new ExpressionBuilderArgument());
+            workflowBuilderKey.Workflow.AddEdge(accumulate, output, new ExpressionBuilderArgument());
+            var workflow2 = workflowBuilderKey.Workflow.Build();
             var observable2 = Expression.Lambda<Func<IObservable<Int64>>>(workflow2).Compile();
             observable2().Subscribe(x => value2 = (int)x);
             value2 += 24;
